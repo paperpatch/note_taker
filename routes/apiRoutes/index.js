cc842dca935db0ce34baa9a97964c47cb2b17be5
paperpatch <patchen21@gmail.com>
2021-11-23 22:59:25 -0500

    working on apiRouting and notes functions

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
-const { validateNotes, createNewNotes } = require('../../lib/notes');
+const { validateNotes, createNewNotes, deleteNotes } = require('../../lib/notes');
 const { notes } = require('../../database/database.json')
 
 router.get('/notes', (req, res) => {
   let results = notes;
-  if (req) {
+  if (!req.body) {
+    res.status(400).send("Notes is not properly formatted.");
+  } else {
     res.json(results);
   }
 })
@@ -18,6 +20,14 @@ router.post('/notes', (req, res) => {
     const note = createNewNotes(req.body, notes);
     res.json(note);
   }
+})
+
+router.delete('/notes', (req, res) => {
+  req.body.id = notes.id;
+})
+
+app.delete('/notes', function (req, res) {
+  res.send('Got a DELETE request at /user')
 })
 
 module.exports = router;
