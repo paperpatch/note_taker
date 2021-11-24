2d960f2683c4a6398d95abe3a23a1146cb8e8593
paperpatch <patchen21@gmail.com>
2021-11-23 19:45:49 -0500

    added library notes.js to connect database. added additional routing

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,3 +1,16 @@
 const router = require('express').Router();
+const { validateNotes } = require('../../lib/notes');
+const { notes } = require('../../database/database.json')
+
+router.post('/notes', (req, res) => {
+  req.body.id = notes.length.toString();
+
+  if (!validateNotes(req.body)) {
+    res.status(400).send('Notes is not properly formatted.');
+  } else {
+    const notes = createNewNotes(req.body, notes);
+    res.json(notes);
+  }
+})
 
 module.exports = router;
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -5,6 +5,10 @@ router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/index.html'));
 })
 
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/notes.html'));
+})
+
 router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
